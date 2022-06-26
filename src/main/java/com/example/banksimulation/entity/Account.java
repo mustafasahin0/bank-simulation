493933f1690054a01d77da7824557f1b8201e8cc
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-06-25 21:12:35 -0600

    Added Account and Transaction classes

---
+++ src/main/java/com/example/banksimulation/entity/Account.java
@@ -0,0 +1,20 @@
+package com.example.banksimulation.entity;
+
+
+import com.example.banksimulation.enums.AccountType;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+public class Account {
+
+    private UUID id;
+    private BigDecimal balance;
+    private AccountType accountType;
+    private Date creationDate;
+    private Long userId;
+
+}
